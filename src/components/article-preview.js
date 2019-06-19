cb7251f465fa19f6ed32d2e70581a0bd4d3fa029
Njeri <njericooper@gmail.com>
2019-06-18 20:32:45 -0500

    styling article cards

--- src/components/article-preview.js
+++ src/components/article-preview.js
@@ -1,20 +1,24 @@
 import React from 'react'
 import Link from 'gatsby-link'
 import Img from 'gatsby-image'
-
-import styles from './article-preview.module.css'
+import styles from './article-preview-card.css'
+/*import styles from './article-preview.module.css'*/
 
 export default ({ article }) => (
-  <div className={styles.preview}>
-    <Img alt="" sizes={article.heroImage.sizes} />
-    <h3 className={styles.previewTitle}>
+  <div className="main-section">
+    <div className="card-container" >
+    <Img className="cardImage" alt="" sizes={article.heroImage.sizes}/>
+      <div className="card-text-container">
+        <span className="card-span"></span>
+    <h1 className="article-title">
       <Link to={`/blog/${article.slug}`}>{article.title}</Link>
-    </h3>
-    <small>{article.publishDate}</small>
-    <p
+    </h1>
+    <p className="article-descrip"
       dangerouslySetInnerHTML={{
         __html: article.description.childMarkdownRemark.html,
       }}
     />
+      </div>
+    </div>
   </div>
 )
