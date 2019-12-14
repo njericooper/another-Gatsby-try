3a972f03f781dd8dbe0fa0368a9effdfa6e92817
njericooper@gmail.com <njericooper@gmail.com>
2019-12-13 23:47:27 -0600

    re-design

--- src/components/article-preview.js
+++ src/components/article-preview.js
@@ -1,8 +1,7 @@
 import React from 'react'
 import Link from 'gatsby-link'
 import Img from 'gatsby-image'
-import styles from './article-preview-card.css'
-/*import styles from './article-preview.module.css'*/
+import styles from './article-preview.module.css'
 
 export default ({ article }) => (
   <div className="main-section">
